ca6939615bafc76925b5de66351e97a7c1dcdaa3
fellowhacker <naveenmanikanta111@gmail.com>
2017-06-30 22:09:03 +0530

    Register completed

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require('express'),
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	expressHandlebars = require('express-handlebars'),
+	expressValidator = require('express-validator'),
+	flash = require('connect-flash'),
+	session = require('express-session'),
+	passport = require('passport'),
+	localStrategy = require('passport-local').Strategy,
+	mongo = require('mongodb'),
+	mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/authen');
+var  db = mongoose.connection;
+
+var routes = require('./routes/index'),
+	users = require('./routes/users');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', expressHandlebars({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended : false}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+	secret: 'secret',
+	saveUninitializes: true,
+	resave: true
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// express validator
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(flash());
+
+app.use(function(req, res, next) {
+
+	res.locals.success_msg = req.flash('Success Messages');
+	res.locals.error_msg = req.flash('Error Message');
+	res.locals.error = req.flash("Error");
+	next();
+
+})
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.set('port', (process.env.PORT || 8080))
+
+app.listen(app.get('port'), function() {
+
+	console.log("Server Started");
+
+})
